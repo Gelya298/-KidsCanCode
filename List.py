7257c23636059801e78168ede965a15bca2ff5fd
Gelya298 <74255879+Gelya298@users.noreply.github.com>
2021-01-14 19:24:29 +0300

    Python variables

---
+++ List.py
@@ -0,0 +1,15 @@
+Python 3.4.2 (v3.4.2:ab2c023a9432, Oct  6 2014, 22:15:05) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> myList = [54, 312, 65, 23]
+>>> friends = ["bob", "sue", "alex"]
+>>> backpack = ["bat eye", "slug slime", "lizard gizzard"]
+>>> backpack[0]
+'bat eye'
+>>> backpack[1]
+'slug slime'
+>>> backpack[2]
+'lizard gizzard'
+>>> backpack[1] = "red slug slime"
+>>> print(backpack)
+['bat eye', 'red slug slime', 'lizard gizzard']
+>>> 
---
+++ Variable.py
@@ -0,0 +1,36 @@
+Python 3.4.2 (v3.4.2:ab2c023a9432, Oct  6 2014, 22:15:05) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> apples = 10
+>>> print(apples)
+10
+>>> apples = 15
+>>> print(apples)
+15
+>>> winner = "Finn"
+>>> print(winner)
+Finn
+>>> flowers = 25
+>>> 3 + 29
+32
+>>> 65 - 3
+62
+>>> 5 * 5
+25
+>>> 50 / 2
+25.0
+>>> apples * 2
+30
+>>> apples + 50
+65
+>>> print("Hello world")
+Hello world
+>>> fruit = "orange"
+>>> print(fruit)
+orange
+>>> hello = "hi" + "there"
+>>> print(hello)
+hithere
+>>> friend = "m" + "a" + "r" + "y"
+>>> print(friend)
+mary
+>>> 
