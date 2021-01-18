2510b7f0aac7df2b6916ffeb50eab156ec64ec72
Gelya298 <74255879+Gelya298@users.noreply.github.com>
2021-01-18 14:10:52 +0300

    Python input

---
+++ Input and Conditional Statements.py
@@ -0,0 +1,13 @@
+name = input("What`s your name? ")
+print("Hello,",name)
+
+ninjas = int(input("How many ninjas are attacking? "))
+
+if ninjas > 50:
+    print("What`s too many!")
+elif ninjas > 20:
+    print("Maybe we can do it!")
+else:
+    print("We can take 'em!")
+    
+    
